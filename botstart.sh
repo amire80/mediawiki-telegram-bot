d31ed9f10c34a23f6205c96d50a05a956eb848c4
Amire80 <amir.aharoni@mail.huji.ac.il>
2016-04-02 15:01:41 +0300

    No need for the path in botstart.sh

--- botstart.sh
+++ botstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jstart -l release=trusty -mem 2g -N aharoni-telegram-bot node ./mediawiki-telegram-bot/MediaWikiTelegramBot.js
+jstart -l release=trusty -mem 2g -N aharoni-telegram-bot node MediaWikiTelegramBot.js
 
