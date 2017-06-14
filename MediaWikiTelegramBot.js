61e3324838b5baeb2a56a5486f7440302b3524a9
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-06-14 15:44:38 +0300

    Add eslint and fix some eslint errors

--- MediaWikiTelegramBot.js
+++ MediaWikiTelegramBot.js
@@ -1,39 +1,16 @@
-var TelegramBot = require( 'node-telegram-bot-api' );
-var request = require( 'request' ).defaults( {
-    jar: true
-} );
-var yaml = require( 'js-yaml' );
-var fs = require( 'fs' );
+"use strict";
 
-var mwApi = require('./MediaWikiAPI.js');
+const TelegramBot = require( 'node-telegram-bot-api' );
+const yaml = require( 'js-yaml' );
+const fs = require( 'fs' );
 
-var mode = 'fetching';
-var apiUrl = 'https://translatewiki.net/w/api.php';
+const mwApi = require('./MediaWikiAPI.js');
 
-var userStatus = {};
-var currentMwMessageIndex = 0;
+let mode = 'fetching';
 
-var config;
+let userStatus = {};
 
-var debug = function ( fromId, info ) {
-    if ( !config.debugLevel ) {
-        return;
-    }
-
-    tgBot.sendMessage( fromId, info );
-};
-
-var getCurrentMwMessage = function ( userID ) {
-    // It will short circuit if you don't check that the object exists
-    if ( !Object.keys(userStatus).length || userStatus[userID].currentMwMessageIndex > userStatus[userID].messages.length ) {
-        userStatus[userID].currentMwMessageIndex = 0;
-        userStatus[userID].messages = [];
-        mode = 'fetching';
-        return null;
-    }
-
-    return userStatus[userID].messages[userStatus[userID].currentMwMessageIndex];
-};
+let config;
 
 // Get document, or throw exception on error
 try {
@@ -44,22 +21,70 @@ try {
     console.log( e );
 }
 
-var tgBot = new TelegramBot( config.token, { polling: true } );
+const tgBot = new TelegramBot( config.token, { polling: true } );
+
+function debug( fromId, info ) {
+    if ( !config.debugLevel ) {
+        return;
+    }
+
+    tgBot.sendMessage( fromId, info );
+}
+
+function getCurrentMwMessage( userID ) {
+    // It will short circuit if you don't check that the object exists
+    if ( !Object.keys(userStatus).length ||
+        userStatus[userID].currentMwMessageIndex > userStatus[userID].messages.length
+    ) {
+        userStatus[userID].currentMwMessageIndex = 0;
+        userStatus[userID].messages = [];
+        mode = 'fetching';
+        return null;
+    }
+
+    return userStatus[userID].messages[userStatus[userID].currentMwMessageIndex];
+}
 
 // Matches /echo [whatever]
 tgBot.onText( /\/echo (.+)/, function ( msg, match ) {
-    var userID = msg.from.id,
-        resp = match[1];
+    const resp = match[1];
+    const userID = msg.from.id;
 
     console.log( msg );
 
     tgBot.sendMessage( userID, resp );
 } );
 
+function getLanguageCode( userID ) {
+    if ( userStatus[userID] === undefined ) {
+        userStatus[userID] = {};
+
+        return '';
+    }
+
+    return userStatus[userID].languageCode;
+}
+
+function setLanguageCode( userID, newLanguageCode ) {
+    debug( userID, "in setLanguageCode(), setting to " +
+        newLanguageCode
+    );
+
+    if ( userStatus[userID] === undefined ) {
+        userStatus[userID] = {};
+    }
+
+    userStatus[userID].languageCode = newLanguageCode;
+    userStatus[userID].currentMwMessageIndex = 0;
+    userStatus[userID].messages = [];
+
+    tgBot.sendMessage( userID, "Set the language code to " + newLanguageCode );
+}
+
 // Matches /setlanguage
 tgBot.onText( /^\/setlanguage ?(.*)/, function ( msg, match ) {
-    var userID = msg.from.id,
-        newLanguageCode = match[1];
+    const newLanguageCode = match[1];
+    const userID = msg.from.id;
 
     debug( userID, "newLanguageCode is " + newLanguageCode );
 
@@ -74,47 +99,16 @@ tgBot.onText( /^\/setlanguage ?(.*)/, function ( msg, match ) {
     setLanguageCode( userID, newLanguageCode );
 } );
 
-var getLanguageCode = function ( userID ) {
-    var languageCode;
-
-    if ( userStatus[userID] === undefined ) {
-        userStatus[userID] = {};
-
-        return '';
-    }
-
-    languageCode = userStatus[userID].languageCode;
-
-    return languageCode;
-};
-
-var setLanguageCode = function ( userID, newLanguageCode ) {
-    debug( userID, "in setLanguageCode(), setting to " +
-        newLanguageCode
-    );
-
-    if ( userStatus[userID] === undefined ) {
-        userStatus[userID] = {};
-    }
-
-    userStatus[userID].languageCode = newLanguageCode;
-    userStatus[userID].currentMwMessageIndex = 0;
-    userStatus[userID].messages = [];
-
-    tgBot.sendMessage( userID, "Set the language code to " + newLanguageCode );
-};
-
 // TODO: Should be much, much more deatiled.
 // For now only checks that it's a string and it's not empty defined.
-var validLanguageCode = function ( languageCode ) {
-    return ( typeof( languageCode ) === 'string' ) && ( languageCode !== '' );
-};
+function validLanguageCode( languageCode ) {
+    return ( typeof languageCode === 'string' ) && ( languageCode !== '' );
+}
 
 // Matches /untranslated
 tgBot.onText( /\/untranslated/, function ( msg, match ) {
-    var newLanguageCode,
-        userID = msg.from.id,
-        languageCode = getLanguageCode( userID );
+    const userID = msg.from.id;
+    let languageCode = getLanguageCode( userID );
 
     if ( !validLanguageCode( languageCode ) ) {
         languageCode = msg.from.language_code;
@@ -137,7 +131,7 @@ tgBot.onText( /\/untranslated/, function ( msg, match ) {
     }
 
     mwApi.getUntranslatedMessages( languageCode, messageCollection => {
-        var currentMwMessage;
+        let currentMwMessage;
 
         debug( userID, 'in getUntranslatedMessages' );
 
@@ -180,10 +174,9 @@ tgBot.onText( /\/untranslated/, function ( msg, match ) {
 
 // Matches anything without a slash in the beginning
 tgBot.onText( /^([^\/].*)/, function ( msg, match ) {
-    var userID = msg.from.id,
-        chatMessage = match[1];
-
-    var targetTranslatableMessage = getCurrentMwMessage( userID );
+    const chatMessage = match[1];
+    const userID = msg.from.id;
+    const targetTranslatableMessage = getCurrentMwMessage( userID );
 
     if ( mode !== 'translation' ||
         targetTranslatableMessage === null
@@ -202,7 +195,7 @@ tgBot.onText( /^([^\/].*)/, function ( msg, match ) {
                 debug( userID, 'Translation published' );
 
                 userStatus[userID].currentMwMessageIndex++;
-                var nextMwMessage = getCurrentMwMessage( userID );
+                const nextMwMessage = getCurrentMwMessage( userID );
 
                 if ( nextMwMessage ) {
                     tgBot.sendMessage(
